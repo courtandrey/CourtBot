b8182717d290061d60bcb99d6489a6f39b2a6815
“courtandrey” <sudarkinandrew@gmail.com>
2022-10-07 15:05:27 +0300

    Moved configuration to Spring

--- src/main/java/courtandrey/courtbot/CourtBotApplication.java
+++ src/main/java/courtandrey/courtbot/CourtBotApplication.java
@@ -1,21 +1,17 @@
 package courtandrey.courtbot;
 
-import courtandrey.courtbot.core.CourtBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import java.util.Map;
 
 @SpringBootApplication
 public class CourtBotApplication {
-    private static final Map<String, String> getenv = System.getenv();
     public static void main(String[] args) {
      try {
-         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-         botsApi.registerBot(new CourtBot(getenv.get("BOT_NAME"), getenv.get("BOT_TOKEN")));
+         new TelegramBotsApi(DefaultBotSession.class);
          SpringApplication.run(CourtBotApplication.class, args);
      } catch (TelegramApiException e) {
          throw new RuntimeException(e);
--- src/main/java/courtandrey/courtbot/core/CourtBot.java
+++ src/main/java/courtandrey/courtbot/core/CourtBot.java
@@ -1,5 +1,6 @@
 package courtandrey.courtbot.core;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -8,21 +9,17 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class CourtBot extends TelegramLongPollingBot {
-    private final String BOT_USERNAME;
-    private final String BOT_TOKEN;
-
-    public CourtBot(String BOT_USERNAME, String BOT_TOKEN) {
-        super();
-        this.BOT_USERNAME = BOT_USERNAME;
-        this.BOT_TOKEN = BOT_TOKEN;
-    }
+    @Value("${BOT_NAME}")
+    private String BOT_USERNAME;
+    @Value("${BOT_TOKEN}")
+    private String BOT_TOKEN;
 
     @Override
     public void onUpdateReceived(Update update) {
         try {
             SendMessage message = new SendMessage();
             message.setChatId(update.getMessage().getChatId());
-            message.setText("Hi");
+            message.setText("Тмок");
             execute(message);
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
